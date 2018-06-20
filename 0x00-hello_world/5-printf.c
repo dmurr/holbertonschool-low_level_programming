d350dcfb4514ba046eda4c1179464fa046269e81
dmurr <diegocmurray@gmail.com>
2018-06-20 21:24:26 +0000

    printf string and compile with betty docs

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Prints string to display
+ *
+ * Return: program does not return anything
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
