ffae7adfa4fec64b04b9140a0370172d3d59140b
dmurr <diegocmurray@gmail.com>
2018-06-21 21:54:11 +0000

    print alphabet in lowercase followed by uppercase

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints letters A through Z with putchar
+ *
+ * Return: 0 is successful
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar (c);
+	for (c = 'A'; c <= 'Z'; c++)
+		putchar (c);
+	putchar('\n');
+	return (0);
+}
