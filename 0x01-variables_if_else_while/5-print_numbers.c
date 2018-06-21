d63ab4e6122cff2b480dabf80ae7b7a2bb9a788a
dmurr <diegocmurray@gmail.com>
2018-06-21 22:01:43 +0000

    print all numbers part of base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints all numbers base 10
+ *
+ * Return: 0 is successful
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		printf("%d", i);
+	printf("\n");
+	return (0);
+}
