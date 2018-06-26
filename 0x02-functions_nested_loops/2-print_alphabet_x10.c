b28dc6c78b797cd6cfb40ca24719327698e1642f
dmurr <diegocmurray@gmail.com>
2018-06-26 22:11:22 +0000

    discarded function call and created loop

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,8 +10,14 @@
 void print_alphabet_x10(void)
 {
 	int i;
-
+	int j;
+	
 	for (i = 0; i <= 10; i++)
-		print_alphabet();
-	_putchar('\n');
+	{
+		for (j = 'a'; j <= 'z'; j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
+	}
 }
