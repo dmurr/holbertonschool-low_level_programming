883c2ebc7ac72536144a051706fcaa51d20f534c
dmurr <diegocmurray@gmail.com>
2018-06-26 22:50:33 +0000

    sum of multiples of 3 and 5 under 1024

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ *
+ *
+ */
+int main(void)
+{
+	int sum;
+	int i;
+
+
+	for (i = 3; i < 1024; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+			sum += i;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
