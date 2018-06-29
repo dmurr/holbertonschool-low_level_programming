577d8b4424b0d42ac2ba68ff3f7669933d5d3005
dmurr <diegocmurray@gmail.com>
2018-06-29 00:16:21 +0000

    prints line lengths of n

---
+++ 0x03-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * print_line - display a line of n length
+ * @n: length of line
+ *
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i <= n; i++)
+		if (n != 0)
+			_putchar('_');
+	_putchar('\n');
+}
