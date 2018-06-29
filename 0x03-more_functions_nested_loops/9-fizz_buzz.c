a5c8dbc2d9eda13b429b204834240ec268d06ed7
dmurr <diegocmurray@gmail.com>
2018-06-29 00:40:08 +0000

    added return explanation in comments

---
+++ 0x03-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - display 1 - 100
+ *
+ * Description: replace numbers divisible by 3 and 5 with "FizzBuzz"
+ * relpace numbers divisible by just 3 with "Fizz"
+ * replace numbers divisible by just 5 with "Buzz"
+ *
+ * Return: 0 always
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz ");
+			continue;
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz ");
+			continue;
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+			continue;
+		}
+		printf("%d ", i);
+	}
+	printf("\n");
+	return (0);
+}
