c28db02a329a7f8dc141af4603cede0e8fc06882
dmurr <diegocmurray@gmail.com>
2018-06-29 06:10:52 +0000

    fixed formating

--- 0x03-more_functions_nested_loops/10-print_triangle.c
+++ 0x03-more_functions_nested_loops/10-print_triangle.c
@@ -15,15 +15,15 @@ void print_triangle(int size)
 	if (size <= 0)
 		_putchar('\n');
 	for (i = 1; i <= size; i++)
-        {
-                for(j = (size - i); j >= 1; j--)
-                {
-                        _putchar(' ');
-                }
+	{
+		for (j = (size - i); j >= 1; j--)
+		{
+			_putchar(' ');
+		}
 		for (k = 1; k <= i; k++)
 		{
 			_putchar('#');
 		}
-                _putchar('\n');
-        }
+		_putchar('\n');
+	}
 }
