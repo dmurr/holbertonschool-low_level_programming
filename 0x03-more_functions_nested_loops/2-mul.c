2dd64c4e79caf5179c88d79477eeb3d8e4d42846
dmurr <diegocmurray@gmail.com>
2018-06-29 00:07:24 +0000

    function that multiplies a and b

---
+++ 0x03-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * mul - multiplies two int inputs
+ * @a: 1st multiplier
+ * @b: 2nd multiplier
+ *
+ * Return: product of two integers
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
