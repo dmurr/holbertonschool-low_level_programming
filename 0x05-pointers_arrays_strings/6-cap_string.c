f1a7759d3ea3309a9b1be6a593ffcc194cc51823
dmurr <diegocmurray@gmail.com>
2018-07-07 21:25:44 +0000

    was skipping index 0 previously

--- 0x05-pointers_arrays_strings/6-cap_string.c
+++ 0x05-pointers_arrays_strings/6-cap_string.c
@@ -10,6 +10,8 @@ char *cap_string(char *str)
 {
 	int i;
 
+	if (str[0] >= 'a' && str[0] <= 'z')
+		str[0] -= 32;
 	for (i = 0; str[i] != '\0'; i++)
 		if ((str[i] == ' ' ||
 		    str[i] == '\t' ||
@@ -24,8 +26,8 @@ char *cap_string(char *str)
 		    str[i] == ')' ||
 		    str[i] == '{' ||
 		    str[i] == '}') &&
-		    (str[i + 1] >= 97 &&
-		     str[i + 1] <= 122))
+		    (str[i + 1] >= 'a' &&
+		     str[i + 1] <= 'z'))
 		{
 			str[i + 1] -= 32;
 		}
