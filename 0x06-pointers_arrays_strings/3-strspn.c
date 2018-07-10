6cb7403e29769b8470e4938a6f83b10dc191d0d8
dmurr <diegocmurray@gmail.com>
2018-07-10 22:14:22 +0000

    counts consecutive bytes of s that match a char in accept

---
+++ 0x06-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ * _strspn - counts initial part of s which consists entirely of bytes in accept
+ * @s: string to be checked for consecutive acceptable characters
+ * @accept: acceptable characters
+ *
+ * Return: number of consecutive bytes of s which consist of bytes of accept
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int i, j;
+	unsigned int bytes = 0;
+
+	for (i = 0; s[i] != '\0' ; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				bytes++;
+				break;
+			}
+		}
+		if (accept[j] == '\0')
+			break;
+	}
+	return (bytes);
+}
