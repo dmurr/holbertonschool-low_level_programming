1af8f4d29417eec942f2bd8b14dcd632d551ad34
dmurr <diegocmurray@gmail.com>
2018-07-11 04:27:01 +0000

    check for no input

--- 0x06-pointers_arrays_strings/5-strstr.c
+++ 0x06-pointers_arrays_strings/5-strstr.c
@@ -13,6 +13,8 @@ char *_strstr(char *haystack, char *needle)
 {
 	int i, j, k;
 
+	if (needle == NULL)
+		return haystack;
 	for (i = 0; i <= (_strlen(haystack) - _strlen(needle)); i++)
 	{
 		if (haystack[i] == needle[0])
