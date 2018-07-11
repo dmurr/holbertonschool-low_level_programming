242a31c5b118df4583df0d7327006a15ea9d115f
dmurr <diegocmurray@gmail.com>
2018-07-11 01:17:21 +0000

    added _strstr prototype

--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -2,10 +2,12 @@
 #define _HOLBERTON_H_
 
 void _putchar(char);
+int _strlen(char *s);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
 
 #endif /* _HOLBERTON_H_ */
