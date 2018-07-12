d69f6dbfdd03a81b68804b75f843ee0c99775860
dmurr <diegocmurray@gmail.com>
2018-07-12 22:05:36 +0000

    finds if string is palindrome using recursion

---
+++ 0x07-recursion/7-is_palindrome.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+
+/**
+ * is_palindrome - checks if string reverser is equal to initial string
+ * @s: string to check
+ *
+ * Return: 1 if is palindrome, 0 if is not
+ */
+int helper(int first, int last, char *s);
+int _strlen_recursion(char *s);
+
+int is_palindrome(char *s)
+{
+
+	return (helper(0, _strlen_recursion(s) - 1, s));
+}
+
+int helper(int first, int last, char *s)
+{
+	if (first >= last)
+		return (1);
+	if (s[first] != s[last])
+		return (0);
+	else
+		return (helper(first + 1, last - 1, s));
+}
+
+int _strlen_recursion(char *s)
+{
+	if (*s != '\0')
+		return (1 + _strlen_recursion(s + 1));
+	return (0);
+}
