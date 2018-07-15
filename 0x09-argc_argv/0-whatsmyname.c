6d16c5a42eebf39e5988a7f5f9950f13edbbed5d
dmurr <diegocmurray@gmail.com>
2018-07-15 00:31:45 +0000

    prints name of executable

---
+++ 0x09-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+#include "stdio.h"
+
+/**
+ * main - prints
+ * @argc: argument count
+ * @argv: argument values
+ *
+ * Return: 0 Always
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
