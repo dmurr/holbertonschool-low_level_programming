20f031c450bf131ebb2b4b7008d540385cedb764
dmurr <diegocmurray@gmail.com>
2018-07-17 17:52:28 +0000

    allocated space for array of any size and initializes

---
+++ 0x0A-malloc_free/0-create_array.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * create_array - allocates space in memory for array and initializes it.
+ * @size: array size to be created
+ * @c: character initializing array
+ *
+ * Return: pointer to string array
+ */
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *s;
+
+	if (size == 0)
+		return (NULL);
+
+	s = malloc(sizeof(char) * size);
+
+	for (i = 0; i < size; i++)
+		s[i] = c;
+
+	return (s);
+}
