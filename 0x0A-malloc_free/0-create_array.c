bfc98cccdfc40440062f490526a93bcbcc68dccb
dmurr <diegocmurray@gmail.com>
2018-07-17 18:54:02 +0000

    moved null checker directly after malloc

--- 0x0A-malloc_free/0-create_array.c
+++ 0x0A-malloc_free/0-create_array.c
@@ -18,11 +18,11 @@ char *create_array(unsigned int size, char c)
 
 	s = malloc(sizeof(char) * size);
 
-	for (i = 0; i < size; i++)
-		s[i] = c;
-
 	if (s == NULL)
 		return (NULL);
+
+	for (i = 0; i < size; i++)
+		s[i] = c;
 
 	return (s);
 }
