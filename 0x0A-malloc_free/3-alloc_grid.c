fadf7ec6c6abaa6f5509413742f8b9d1408392b3
dmurr <diegocmurray@gmail.com>
2018-07-18 01:20:15 +0000

    switched widths and heights

--- 0x0A-malloc_free/3-alloc_grid.c
+++ 0x0A-malloc_free/3-alloc_grid.c
@@ -17,14 +17,14 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	arr = (int **)malloc(sizeof(int *) * width);
+	arr = (int **)malloc(sizeof(int *) * height);
 
 	if (arr == NULL)
 		return (NULL);
 
 	for (i = 0; i < width; i++)
 	{
-		arr[i] = (int *)malloc(sizeof(int) * height);
+		arr[i] = (int *)malloc(sizeof(int) * width);
 
 		if (arr[i] == NULL)
 		{
@@ -37,10 +37,9 @@ int **alloc_grid(int width, int height)
 		}
 	}
 
-	for (i = 0; i < width; i++)
-		for (j = 0; j < height; j++)
+	for (i = 0; i < height; i++)
+		for (j = 0; j < width; j++)
 			arr[i][j] = 0;
 
 	return (arr);
-	free(arr);
 }
