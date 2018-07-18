5b44dbc3c53fd7db7e51e06aa60fbfe3baab3010
dmurr <diegocmurray@gmail.com>
2018-07-18 01:50:53 +0000

    betty

--- 0x0A-malloc_free/4-free_grid.c
+++ 0x0A-malloc_free/4-free_grid.c
@@ -3,11 +3,11 @@
 #include <stdlib.h>
 
 /**
+ * free_grid - frees 2D array from memory
+ * @grid: 2D array
+ * @height: height of array
  *
- *
- *
- *
- *
+ * Return: Nothing
  */
 void free_grid(int **grid, int height)
 {
