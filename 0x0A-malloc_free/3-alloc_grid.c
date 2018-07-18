5c61ca1436fadba42cc14671c6fc1ea553db4e6e
dmurr <diegocmurray@gmail.com>
2018-07-18 00:59:22 +0000

    Allocate memory for a 2D array

---
+++ 0x0A-malloc_free/3-alloc_grid.c
@@ -0,0 +1,46 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * alloc_grid - allocated 2D array in memory
+ * @width:
+ * @length:
+ *
+ * Return:
+ */
+int **alloc_grid(int width, int height)
+{
+	int i, j;
+	int **arr;
+
+	if (width < 0 || height < 0)
+		return (NULL);
+
+	arr = (int **)malloc(sizeof(int *) * width);
+
+	if (arr == NULL)
+		return (NULL);
+
+	for (i = 0; i < width; i++)
+	{
+		arr[i] = (int *)malloc(sizeof(int) * height);
+
+		if (arr[i] == NULL)
+		{
+			while (i >= 0)
+			{
+				free(arr[i]);
+				i--;
+			}
+			return (NULL);
+		}
+
+	}
+
+	for (i = 0; i < width; i++)
+		for (j = 0; j < height; j++)
+			arr[i][j] = 0;
+
+	return (arr);
+}
