57142261008fa25f385c75fd50f5118462a10978
dmurr <diegocmurray@gmail.com>
2018-07-19 23:18:47 +0000

    added prototype for new function: _calloc

--- 0x0B-more_malloc_free/holberton.h
+++ 0x0B-more_malloc_free/holberton.h
@@ -4,6 +4,7 @@
 
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
 
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
