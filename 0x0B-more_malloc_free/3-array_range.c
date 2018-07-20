bcfa14f3109e498534473139061af89a3c08dcf1
dmurr <diegocmurray@gmail.com>
2018-07-20 01:34:53 +0000

    allocated array for range of numbers and fills values

---
+++ 0x0B-more_malloc_free/3-array_range.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * array_range - contains all values from min to max
+ * @min: minimum
+ * @max: maximum
+ *
+ * Return: pointer to newly created array
+ */
+int *array_range(int min, int max)
+{
+	int *arr;
+	int i;
+	int j = 0;
+
+	if (min > max)
+		return (NULL);
+
+	arr = malloc(sizeof(int) * (max - min + 1));
+
+	for (i = min; i < (max + 1); i++)
+	{
+		arr[j] = i;
+		j++;
+	}
+
+	return (arr);
+}
