48b4c0e6e3863ba6043cf3ec6f54187c2b450c08
dmurr <diegocmurray@gmail.com>
2018-07-23 23:52:22 +0000

    initialize struct in void function

---
+++ 0x0D-structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+
+/**
+ * init_dog - initializes struct dog
+ * @d: dog struct
+ * @name: name of doggo
+ * @age: age of doggo
+ * @owner: owner of doggo
+ *
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if(!d)
+		return;
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
