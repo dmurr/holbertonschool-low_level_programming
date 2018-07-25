6c8a163f94898be6a2fbae84f607480b967a048d
dmurr <diegocmurray@gmail.com>
2018-07-25 17:08:35 +0000

    func that iterates through array and points to function for each index

---
+++ 0x0E-function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * array_iterator - iterates through array and calls function on each index.
+ * @array: array to iterate through
+ * @size: number of elements in array
+ * @f: pointer to function
+ *
+ * Return: Nothing.
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	if (array == NULL || action == NULL)
+		return;
+
+	for (i = 0; i < size; i++)
+		action(array[i]);
+}
