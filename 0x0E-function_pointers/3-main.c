52fcdc11c9b466d29e17730b6aaf82dc0497a44a
dmurr <diegocmurray@gmail.com>
2018-07-26 01:33:43 +0000

    calculator

---
+++ 0x0E-function_pointers/3-main.c
@@ -0,0 +1,43 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "3-calc.h"
+
+/**
+ * main - performs various opertation on arguments passed at runtime
+ * @argc: arguments count
+ * @argv: argument values
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int result;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	if ((strcmp(argv[2], "+") != 0) &&
+	    (strcmp(argv[2], "-") != 0) &&
+	    (strcmp(argv[2], "*") != 0) &&
+	    (strcmp(argv[2], "/") != 0) &&
+	    (strcmp(argv[2], "%") != 0))
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	if ((strcmp(argv[2], "/") == 0  && argv[3] == 0) ||
+	    (strcmp(argv[2], "%") && argv[3] == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	result = (*get_op_func(argv[2]))(atoi(argv[1]), atoi(argv[3]));
+
+	printf("%d\n", result);
+
+	return (0);
+}
---
+++ 0x0E-function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include <stdlib.h>
+
+/**
+ * op_add - adds two numbers
+ * @a: number 1
+ * @b: number 2
+ *
+ * Return: sum
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtracts two numbers
+ * @a: minuend
+ * @b: subtruend
+ *
+ * Return: difference
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplies two numbers
+ * @a: number 1
+ * @b: number 2
+ *
+ * Return: product
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divied two numbers
+ * @a: dividend
+ * @b: divisor
+ *
+ * Return: Quotient
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - performs modulo operation
+ * @a: number 1
+ * @b: number 2
+ *
+ * Return: remainder
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
