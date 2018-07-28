498dbfcc1f2e774f73a109a3b64d141f8276fe83
dmurr <diegocmurray@gmail.com>
2018-07-28 21:34:01 +0000

    switch != to < in last condition

--- 0x0F-variadic_functions/2-print_strings.c
+++ 0x0F-variadic_functions/2-print_strings.c
@@ -25,7 +25,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 				printf("%s", str);
 			else
 				printf("(nil)");
-			if (separator && i != n - 1)
+			if (separator && i < n - 1)
 				printf("%s", separator);
 		}
 		va_end(valist);
