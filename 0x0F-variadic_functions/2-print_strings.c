6bda36323a572444da7e45e780567c862fc13d45
dmurr <diegocmurray@gmail.com>
2018-07-28 20:39:31 +0000

    prints strings follow by a new line

---
+++ 0x0F-variadic_functions/2-print_strings.c
@@ -0,0 +1,33 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * print_strings - prints string arguments
+ * @separator: separates arguments when printed
+ * @n: number of arguments
+ *
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list valist;
+	unsigned int i;
+
+	if (!n)
+		return;
+
+	va_start(valist, n);
+	for (i = 0; i < n; i++)
+	{
+		char *str = va_arg(valist, char*);
+		if (str)
+			printf("%s", str);
+		else
+			printf("(nil)");
+		if (separator && i != n - 1)
+			printf("%s", separator);
+	}
+	va_end(valist);
+	printf("\n");
+}
