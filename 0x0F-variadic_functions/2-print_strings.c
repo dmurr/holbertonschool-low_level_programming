11a59c7a474fb4b50c6c5513f39e4f82f544f0d3
dmurr <diegocmurray@gmail.com>
2018-07-28 21:23:59 +0000

    removed null checker return null

--- 0x0F-variadic_functions/2-print_strings.c
+++ 0x0F-variadic_functions/2-print_strings.c
@@ -14,20 +14,21 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	va_list valist;
 	unsigned int i;
 
-	if (!n)
-		return;
-
-	va_start(valist, n);
-	for (i = 0; i < n; i++)
+	if (n)
 	{
-		char *str = va_arg(valist, char*);
-		if (str)
-			printf("%s", str);
-		else
-			printf("(nil)");
-		if (separator && i != n - 1)
-			printf("%s", separator);
-	}
+		va_start(valist, n);
+		for (i = 0; i < n; i++)
+		{
+			char *str = va_arg(valist, char*);
+
+			if (str)
+				printf("%s", str);
+			else
+				printf("(nil)");
+			if (separator && i != n - 1)
+				printf("%s", separator);
+		}
 	va_end(valist);
 	printf("\n");
+	}
 }
