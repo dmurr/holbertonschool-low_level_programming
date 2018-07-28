00c73a53da81785e4848a06cce5e9a0bd27d4047
dmurr <diegocmurray@gmail.com>
2018-07-28 21:32:44 +0000

    indented last line

--- 0x0F-variadic_functions/2-print_strings.c
+++ 0x0F-variadic_functions/2-print_strings.c
@@ -28,7 +28,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			if (separator && i != n - 1)
 				printf("%s", separator);
 		}
-	va_end(valist);
-	printf("\n");
+		va_end(valist);
+		printf("\n");
 	}
 }
