9770c16d6b66869ceb6b87375faa7038630bc7c3
dmurr <diegocmurray@gmail.com>
2018-08-02 21:23:32 +0000

    prints singly linked list values and returns number of nodes

---
+++ 0x11-singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * print_list - prints linked list
+ * @h: pointer to first node in list
+ *
+ * Return: number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	size_t nodeCount = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		nodeCount++;
+		h = h->next;
+	}
+	return (nodeCount);
+}
