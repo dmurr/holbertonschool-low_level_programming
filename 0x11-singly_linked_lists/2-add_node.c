e1177f71c4cdf10f92dff54e03410c273a80b880
dmurr <diegocmurray@gmail.com>
2018-08-03 04:33:37 +0000

    adds note to linked list

---
+++ 0x11-singly_linked_lists/2-add_node.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+
+/**
+ * add_node - adds node to linked list
+ * @head: pointer to head
+ * @str:
+ *
+ * Return: address of new element or null
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *temp = NULL;
+	list_t *node;
+	int i;
+
+	if (!head || !str)
+		return (NULL);
+
+	node = malloc(sizeof(list_t));
+	if (node == NULL)
+		return (NULL);
+
+	while (str[i] != '\0')
+		i++;
+
+	node->str = strdup(str);
+	node->len = i;
+	node->next = NULL;
+
+
+	temp = *head;
+	*head = node;
+	node->next = temp;
+
+	return (node);
+
+}
