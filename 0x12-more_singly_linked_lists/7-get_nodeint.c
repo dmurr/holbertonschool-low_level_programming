98bbebc72f8c1ec5323cdda2a78aa7a5c8e9fdef
dmurr <diegocmurray@gmail.com>
2018-08-07 21:05:58 +0000

    testing if null checker on head is necessary

--- 0x12-more_singly_linked_lists/7-get_nodeint.c
+++ 0x12-more_singly_linked_lists/7-get_nodeint.c
@@ -12,7 +12,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	listint_t *temp;
 	unsigned int i = 0;
 
-	if (!head || !index)
+	if (!index)
 		return (NULL);
 
 	temp = head;
