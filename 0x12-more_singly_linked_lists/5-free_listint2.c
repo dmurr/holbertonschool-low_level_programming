dc7601b8bf7db483ee65fc3e764c0dff54ab1728
dmurr <diegocmurray@gmail.com>
2018-08-07 20:13:48 +0000

    function frees linked list from double pointer

---
+++ 0x12-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees list from double pointer
+ * @head: points to list
+ *
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	while (*head != NULL)
+	{
+		temp = *head;
+		*head = (*head)->next;
+		free(temp);
+	}
+}
