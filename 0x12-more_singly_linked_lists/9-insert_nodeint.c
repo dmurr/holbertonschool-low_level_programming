8e270ca15b038f4ed9f433f9cf4b6a6284683a63
dmurr <diegocmurray@gmail.com>
2018-08-08 03:30:36 +0000

    inserts new node in list

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,6 +1,24 @@
 #include "lists.h"
 
 /**
+ * listint_len - finds length of linked list
+ * @h: head of linked list
+ *
+ * Return: length
+ */
+size_t listint_len(const listint_t *h)
+{
+	int nodeCount = 0;
+
+	while (h != NULL)
+	{
+		nodeCount++;
+		h = h->next;
+	}
+	return (nodeCount);
+}
+
+/**
  * insert_nodeint_at_index - inserts node at index idx with value n
  * @head: pointer to list head
  * @idx: desired index for new node
@@ -20,7 +38,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 
 	temp = *head;
-
 	if (!temp)
 		return (NULL);
 
@@ -31,8 +48,26 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		temp = temp->next;
 		i++;
 	}
-	prev->next = node;
-	node->next = temp;
+
+	if (idx == 0)
+	{
+		node->next = *head;
+		*head = node;
+	}
+	else if (idx < listint_len(*head))
+	{
+		node->next = temp;
+		prev->next = node;
+	}
+	else if (idx == listint_len(*head))
+	{
+		temp->next = node;
+		node->next = NULL;
+	}
+	else
+		return (NULL);
+
 	node->n = n;
+
 	return (node);
 }
