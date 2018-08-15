1bd8d2980317b97465cbf7a58c5c801a5481c720
dmurr <diegocmurray@gmail.com>
2018-08-15 22:05:04 +0000

    Changed return of file descriptor checker to -1

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -22,7 +22,7 @@ int create_file(const char *filename, char *text_content)
 
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
-		return (0);
+		return (-1);
 
 	if (!text_content)
 	{
