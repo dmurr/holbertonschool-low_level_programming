c1c31e7bb5fe3fd3f60b87e49db2ca6e1629e24a
dmurr <diegocmurray@gmail.com>
2018-08-15 22:01:41 +0000

    Function creates a file, truncates if it already exists

---
+++ 0x14-file_io/1-create_file.c
@@ -0,0 +1,42 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+
+/**
+ * create_file - creates a file
+ * @filename: file to be created
+ * @text_content: content to be copied into new file
+ *
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	size_t len;
+	ssize_t w;
+
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (fd == -1)
+		return (0);
+
+	if (!text_content)
+	{
+		close(fd);
+		return (1);
+	}
+
+	for (len = 0; text_content[len] != '\0'; len++)
+		;
+
+	w = write(fd, text_content, len);
+	if (w == -1)
+		return (-1);
+
+	close(fd);
+	return (1);
+}
