4a3c28ef7762eabd9d563ab0a284530f33d3f738
dmurr <diegocmurray@gmail.com>
2018-09-16 16:32:50 +0000

    Added prototype: dlistint_len, and included guards

--- 0x16-doubly_linked_lists/lists.h
+++ 0x16-doubly_linked_lists/lists.h
@@ -1,3 +1,6 @@
+#ifndef _LISTS_H
+#define _LISTS_H_
+
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
@@ -19,3 +22,6 @@ typedef struct dlistint_s
 } dlistint_t;
 
 size_t print_dlistint(const dlistint_t *h);
+size_t dlistint_len(const dlistint_t *h);
+
+#endif /* _LISTS_H_ */
