71d80c462fa724ae175b7c977a92d15678d63469
dmurr <diegocmurray@gmail.com>
2018-09-16 17:24:58 +0000

    Made code easier to read and set new nodes prev to proper value

--- 0x16-doubly_linked_lists/2-add_dnodeint.c
+++ 0x16-doubly_linked_lists/2-add_dnodeint.c
@@ -2,14 +2,17 @@
 
 /**
  * add_dnodeint - adds node to beginning  of list
- * @head: list
- * @n: node value
+ * @head: pointer to head of list
+ * @n: value of node
  *
- * Return: new node
+ * Return: address of new node or null if it fails
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *newnode;
+
+	if (!head)
+		return (NULL);
 
 	newnode = malloc(sizeof(dlistint_t));
 	if (!newnode)
@@ -17,15 +20,11 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 
         newnode->n = n;
         newnode->prev = NULL;
-
-	if (head == NULL)
-	{
-		*head = newnode;
-		newnode->next = NULL;
-		return (newnode);
-	}
-
 	newnode->next = *head;
+
+	if (*head)
+		(*head)->prev = newnode;
+
 	*head = newnode;
 	return (newnode);
 
