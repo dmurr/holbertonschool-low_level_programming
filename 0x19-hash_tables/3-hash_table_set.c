0cca3de0d1bdca140a429b16c3cd427c7c9814c5
dmurr <diegocmurray@gmail.com>
2018-10-12 20:42:41 +0000

    Initial commit: function adds element to hash table

---
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -0,0 +1,56 @@
+#include "hash_tables.h"
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int idx;
+	hash_node_t *temp;
+
+	if (!*key || strcmp((const char *)key, "") == 0)
+		return (0);
+
+	idx = key_index((unsigned char *)key, ht->size);
+
+	temp = ht->array[idx];
+	while(temp != NULL)
+	{
+		if (strcmp(temp->key, key) == 0)
+		{
+			free(temp->value);
+			temp->value = strdup(value);
+			if (temp->value == NULL)
+			{
+				free(temp->value);
+				return (0);
+			}
+			return (1);
+		}
+		temp = temp->next;
+	}
+	if (add_node(&ht->array[idx], key, value) == NULL)
+		return (0);
+
+	return (1);
+}
+
+hash_node_t *add_node(hash_node_t **head, const char *key, const char *value)
+{
+	hash_node_t *new = NULL;
+
+	new = malloc(sizeof(hash_node_t *));
+
+	new->key = strdup(key);
+	if (new->key == NULL)
+		return (NULL);
+        new->value = strdup(value);
+	if (new->value == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+	new->next = NULL;
+	*head = new;
+	return (*head);
+}
