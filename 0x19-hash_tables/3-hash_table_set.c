783ed140eff3929400ea29cb7f46be6f45d104af
dmurr <diegocmurray@gmail.com>
2018-10-12 20:43:25 +0000

    testing

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -8,7 +8,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int idx;
 	hash_node_t *temp;
 
-	if (!*key || strcmp((const char *)key, "") == 0)
+	if (!*key)
 		return (0);
 
 	idx = key_index((unsigned char *)key, ht->size);
