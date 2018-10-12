02677db2f8f992416fee78e420c4ea7661d108b7
dmurr <diegocmurray@gmail.com>
2018-10-12 20:49:25 +0000

    removed checks

--- 0x19-hash_tables/2-key_index.c
+++ 0x19-hash_tables/2-key_index.c
@@ -9,11 +9,5 @@
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	unsigned long int idx;
-
-	if (size == 0 || !*key || strcmp((const char *)key, "") == 0)
-		return (0);
-
-	idx = hash_djb2(key) % size;
-	return (idx);
+	return (hash_djb2(key) % size);
 }
