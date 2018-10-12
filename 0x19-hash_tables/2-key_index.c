b350d1917805f09296b1e904867cc60aa757cea1
dmurr <diegocmurray@gmail.com>
2018-10-12 20:40:47 +0000

    fixed styling error

--- 0x19-hash_tables/2-key_index.c
+++ 0x19-hash_tables/2-key_index.c
@@ -11,7 +11,7 @@ unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	unsigned long int idx;
 
-	if (size == 0 || !*key ||strcmp((const char *)key, "") == 0)
+	if (size == 0 || !*key || strcmp((const char *)key, "") == 0)
 		return (0);
 
 	idx = hash_djb2(key) % size;
