a7691e72b3082fbb2f103cc5b1cfa2561bcf519c
dmurr <diegocmurray@gmail.com>
2018-10-29 18:52:23 +0000

    removed swap checker

--- 0x1A-sorting_algorithms/0-bubble_sort.c
+++ 0x1A-sorting_algorithms/0-bubble_sort.c
@@ -17,7 +17,6 @@ void bubble_sort(int *array, size_t size)
 
 	for (i = 0; i < size - 1; i++)
 	{
-		swapped = 0;
 		for (j = 0; j < size - i - 1; j++)
 		{
 			if (array[j] > array[j + 1])
@@ -25,11 +24,8 @@ void bubble_sort(int *array, size_t size)
 				temp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
-				swapped = 1;
 				print_array(array, size);
 			}
 		}
-		if (swapped == 0)
-			break;
 	}
 }
