5dee84a5b111bdaba8e175a4f4cf001afb72b1c7
dmurr <diegocmurray@gmail.com>
2018-10-30 23:16:33 +0000

    inital commit

---
+++ 0x1A-sorting_algorithms/2-selection_sort.c
@@ -0,0 +1,34 @@
+#include "sort.h"
+
+/**
+ * selection_sort -
+ * @array: the array
+ * @size: size of array
+ *
+ */
+void selection_sort(int *array, size_t size)
+{
+	unsigned int i, j;
+	int select_val, select_idx;
+
+	if (!*array || size == 0)
+		return;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		select_val = array[i];
+
+		for (j = i; j < size; j++)
+		{
+			if (array[j] < select_val)
+			{
+				select_idx = j;
+				select_val = array[j];
+			}
+		}
+		array[select_idx] = array[i];
+		array[i] = select_val;
+		print_array(array,size);
+	}
+
+}
