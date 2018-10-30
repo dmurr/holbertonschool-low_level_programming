a838885e05e08de1eb9d07e1a3a70cabc28373fb
dmurr <diegocmurray@gmail.com>
2018-10-30 17:51:53 +0000

    fixed swapping logic

--- 0x1A-sorting_algorithms/0-bubble_sort.c
+++ 0x1A-sorting_algorithms/0-bubble_sort.c
@@ -21,8 +21,8 @@ void bubble_sort(int *array, size_t size)
 			if (array[j] < array[j - 1])
 			{
 				temp = array[j];
-				array[j] = array[j + 1];
-				array[j + 1] = temp;
+				array[j] = array[j - 1];
+				array[j - 1] = temp;
 				print_array(array, size);
 			}
 		}
