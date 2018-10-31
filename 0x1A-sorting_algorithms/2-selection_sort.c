a0b69a1d7d6298787f8931dc5e1ffc4262794ac7
dmurr <diegocmurray@gmail.com>
2018-10-31 00:15:13 +0000

    No longer swaps with itself

--- 0x1A-sorting_algorithms/2-selection_sort.c
+++ 0x1A-sorting_algorithms/2-selection_sort.c
@@ -8,27 +8,28 @@
  */
 void selection_sort(int *array, size_t size)
 {
-	unsigned int i, j;
-	int min_idx, temp;
+	unsigned int i, j, min_idx, temp;
 
-	if (!*array || !array || size <= 1)
+	if (size <= 1)
 		return;
 
 	for (i = 0; i < size - 1; i++)
 	{
 		min_idx = i;
 
-		for (j = i; j < size; j++)
+		for (j = i + 1; j < size; j++)
 		{
 			if (array[j] < array[min_idx])
 				min_idx = j;
 		}
 
-		temp = array[i];
-		array[i] = array[min_idx];
-		array[min_idx] = temp;
-
-		print_array(array, size);
+		if (i != min_idx)
+		{
+			temp = array[i];
+			array[i] = array[min_idx];
+			array[min_idx] = temp;
+			print_array(array, size);
+		}
 	}
 
 }
