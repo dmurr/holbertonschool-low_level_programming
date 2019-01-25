d4bfb431287b29c0b01799b117f1d57280e8cc9a
dmurr <diegocmurray@gmail.com>
2019-01-25 04:57:26 +0000

    removed comma after last element during search

--- 0x1D-search_algorithms/1-binary.c
+++ 0x1D-search_algorithms/1-binary.c
@@ -18,9 +18,9 @@ int binary_search(int *array, size_t size, int value)
 	while (low <= high)
 	{
 		printf("Searching in array: ");
-		for (i = low; i <= high; i++)
+		for (i = low; i < high; i++)
 			printf("%d, ", array[i]);
-		printf("\n");
+		printf("%d\n", array[high]);
 
 		mid = (low + high) / 2;
 		if (array[mid] == value)
