fd4bc0bbd65a88797afa97f96402dae66481370e
dmurr <diegocmurray@gmail.com>
2019-04-18 05:47:13 +0000

    altered logic

--- 0x1D-search_algorithms/100-jump.c
+++ 0x1D-search_algorithms/100-jump.c
@@ -54,12 +54,11 @@ int jump_search(int *array, size_t size, int value)
 
 	right = min(size - 1, right);
 
-	for (i = left; i <= right; i++)
+	for (i = left; i <= right && array[i] <= value; i++)
 	{
 		printf("Value checked array[%d] = [%d]\n", left, array[i]);
 		if (array[i] == value)
-			return (i);
+			break;
 	}
-
-	return (-1);
+	return (i);
 }
