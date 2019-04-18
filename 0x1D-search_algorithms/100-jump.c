9556a1f83aba47eb2a0e34cceeb8bcc7749044a2
dmurr <diegocmurray@gmail.com>
2019-04-18 05:10:21 +0000

    Implementation of jump search algorithm

---
+++ 0x1D-search_algorithms/100-jump.c
@@ -0,0 +1,68 @@
+#include "search_algos.h"
+#include <math.h>
+
+/**
+ * min - finds the mininum value
+ * @a: value
+ * @b: another value
+ * Return: minimum value
+ */
+int min(int a, int b)
+{
+	return (a < b ? a : b);
+}
+
+
+/**
+ * jump_search - searches sorted array using jump sort algorithm
+ * @array: pointer to the first index in array
+ * @size: number of elements in the array
+ * @value: value to be searched for
+ * Return: First index where value is found or -1
+ */
+int jump_search(int *array, size_t size, int value)
+{
+	int jump = (int)sqrt(size);
+	unsigned int left = 0;
+	unsigned int right = 0;
+	unsigned int i = 0;
+
+	if(!array)
+		return(-1);
+
+	/* Searches by blocks rather than 1-by-1
+	   Breaks once block contains value
+	*/
+	while (left < size && array[left] <= value)
+	{
+		right = min(size - 1, left + jump);
+
+		printf("Value checked array[%d] = [%d]\n", left, array[left]);
+
+		if (array[left] <= value && array[right] >= value)
+		{
+			printf("Value found between indexes [%d] and [%d]\n"
+			       , left, right);
+			break;
+		}
+
+		left += jump;
+	}
+
+	if (left >= size || array[left] > value)
+	{
+		return (-1);
+	}
+
+	/* if jump passed max index in array set right to max */
+	right = min(size - 1, right);
+
+	for (i = left; i <= right; i++)
+	{
+		printf("Value checked array[%d] = [%d]\n", left, array[i]);
+		if (array[i] == value)
+			break;
+	}
+
+	return (i);
+}
